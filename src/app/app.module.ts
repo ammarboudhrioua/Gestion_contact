00a3064e62de074290bfea2ab5271d381bb17e10
ammarboudhrioua <ammar.boudhrioua@gmail.com>
2024-02-06 00:28:13 +0100

    ng-bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { NgBootstrapWrapperModule } from './wrappers/ng-bootstrap/ng-bootstrap-wrapper/ng-bootstrap-wrapper.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgBootstrapWrapperModule
   ],
   providers: [
     provideClientHydration()
